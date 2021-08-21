10c8b4fa0cf8a321dfc7399ca08a15cdedc89f44
artur <artur.salviano@gmail.com>
2021-08-20 21:20:57 -0300

    Criado atributos de instancias

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=28):
+        self.idade = idade
+        self.nome = nome
 
     def comprimentar(self):
         return f'Olá {id(self)}'
 
-if __name__ =='__main__':
-    p = Pessoa()
+
+if __name__ == '__main__':
+    p = Pessoa('Iury')
     print(Pessoa.comprimentar(p))
     print(id(p))
     print(p.comprimentar())
+    print(p.nome)
+    p.nome = 'Artur'
+    print(p.nome)
+    print(p.idade)
