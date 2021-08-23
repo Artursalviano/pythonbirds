d2f437e7bd12808a20844e5ba9774ab0755661e0
artur <artur.salviano@gmail.com>
2021-08-23 19:48:12 -0300

    Criado os atributos de classes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=28):
         self.idade = idade
         self.nome = nome
@@ -9,12 +10,20 @@ class Pessoa:
 
 if __name__ == '__main__':
     iury = Pessoa(nome ='Iury')
-    artur = Pessoa(iury, nome ='Artur')
     ana = Pessoa( nome = 'Ana')
+    artur = Pessoa(ana,iury, nome ='Artur')
+    
     print(Pessoa.comprimentar(iury))
     print(id(iury))
     print(iury.comprimentar())
     print(iury.nome)
     for filho in artur.filhos:
         print(filho.nome)
-
+    iury.olhos = 1
+    print(artur.__dict__)
+    print(iury.__dict__)
+    
+    print(Pessoa.olhos)
+    print(iury.olhos)
+    print(artur.olhos)
+    print(id(Pessoa.olhos), id(iury.olhos),id(artur.olhos))
